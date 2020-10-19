574a82ddb0cc60159736f4c9e3586b1d78fe1ced
patrick.barcelos <patrick.barcelos@orgsystem.com.br>
2020-10-19 13:35:52 -0300

    changes in table css and test

--- frontend/src/app/components/transaction/transaction-table/transaction-table.component.spec.ts
+++ frontend/src/app/components/transaction/transaction-table/transaction-table.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { TransactionTableComponent } from './transaction-table.component';
 
 describe('TransactionTableComponent', () => {
@@ -8,7 +9,8 @@ describe('TransactionTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TransactionTableComponent ]
+      declarations: [ TransactionTableComponent ],
+      imports: [ HttpClientModule, MatSnackBarModule ]
     })
     .compileComponents();
   });
